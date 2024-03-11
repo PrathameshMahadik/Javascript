7ce2f474c0cec1ed6a17a5233fdc09e5cd9a7a51
PrathameshMahadik <prathamesh.mahadik@successive.tech>
2024-03-11 15:16:57 +0530

    Changes Updated

--- feature/day4/Task7.js
+++ feature/day4/Task7.js
@@ -2,10 +2,10 @@
 let arr = [10, 20, 30, 10, 30, 40, 50, 60, 10, 60];
 let hashMap = new Map();
 for (let i = 0; i < arr.length; i++) {
-  if(!hashMap.has(arr[i])){
-    hashMap.set(arr[i],1);
-  }else{
-    hashMap.set(arr[i],hashMap.get(arr[i])+1);
+  if (!hashMap.has(arr[i])) {
+    hashMap.set(arr[i], 1);
+  } else {
+    hashMap.set(arr[i], hashMap.get(arr[i]) + 1);
     console.log(i);
   }
 }
--- feature/day4/Task9.js
+++ feature/day4/Task9.js
@@ -3,9 +3,9 @@ let arr = [10, 20, 30, 10, 30, 40, 50, 60, 10, 60];
 let index = [];
 for (let i = 0; i < arr.length; i++) {
   for (let j = i + 1; j < arr.length; j++) {
-    if (arr[j] === arr[i] && index.indexOf(arr[j])===-1) {
+    if (arr[j] === arr[i]) {
       index.push(j);
     }
   }
 }
-console.log(index[index.length-1]);
+console.log(index[index.length - 1]);
