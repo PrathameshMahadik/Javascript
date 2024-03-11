7a560d59c29f768bfd6d7ddd0fe61b7d6306131a
PrathameshMahadik <prathamesh.mahadik@successive.tech>
2024-03-11 15:08:15 +0530

    Changes Updated

--- feature/day4/Task7.js
+++ feature/day4/Task7.js
@@ -1,20 +1,11 @@
 // 7. Write a program to find index of duplicate elements in an array
 let arr = [10, 20, 30, 10, 30, 40, 50, 60, 10, 60];
-let index = [];
-for (var i = 0; i < arr.length; i++) {
-  for (var j = i + 1; j < arr.length; j++) {
-    if (arr[j] === arr[i]) {
-      index.push(j);
-    }
+let hashMap = new Map();
+for (let i = 0; i < arr.length; i++) {
+  if(!hashMap.has(arr[i])){
+    hashMap.set(arr[i],1);
+  }else{
+    hashMap.set(arr[i],hashMap.get(arr[i])+1);
+    console.log(i);
   }
 }
-function removeDuplicates(index) {
-  let result = [];
-  for (i = 0; i < index.length; i++) {
-      if (result.indexOf(index[i]) === -1) {
-          result.push(index[i]);
-      }
-  }
-  return result;
-}
-console.log(removeDuplicates(index));
--- feature/day4/Task9.js
+++ feature/day4/Task9.js
@@ -1,20 +1,11 @@
 // 9. Write a program to find the last duplicate index in an array
 let arr = [10, 20, 30, 10, 30, 40, 50, 60, 10, 60];
 let index = [];
-for (var i = 0; i < arr.length; i++) {
-  for (var j = i + 1; j < arr.length; j++) {
-    if (arr[j] === arr[i]) {
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[j] === arr[i] && index.indexOf(arr[j])===-1) {
       index.push(j);
     }
   }
 }
-function removeDuplicates(index) {
-  let result = [];
-  for (i = 0; i < index.length; i++) {
-      if (result.indexOf(index[i]) === -1) {
-          result.push(index[i]);
-      }
-  }
-  return result;
-}let a = removeDuplicates(index)
-console.log(a[a.length-1]);
+console.log(index[index.length-1]);
