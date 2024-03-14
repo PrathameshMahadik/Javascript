17d3e39a5d615ef3bfde5b76bdb043db0e8a9cb2
PrathameshMahadik <prathamesh.mahadik@successive.tech>
2024-03-14 17:44:57 +0530

    Updated Task 3 & 4

--- feature/day6/Task4.js
+++ feature/day6/Task4.js
@@ -2,31 +2,30 @@
 that returns the person's full name.
 Additionally, the class should have a method called `averageAge` that takes in an array of `Person` objects 
 and returns the average age of all the people in the array.*/
-class Person{
-    constructor(firstName, lastName, age){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.age=age;
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  fullName() {
+    console.log(`Full Name: ${this.firstName} ${this.lastName}`);
+  }
+  averageAge(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i].age;
     }
-    fullName(){
-        console.log(`Full Name: ${this.firstName} ${this.lastName}`);
-    }
-    averageAge(arr){
-        var sum = 0;
-        for(let i=0;i<arr.length;i++){
-            sum+=arr[i].age;
-        }
-        return (sum/arr.length);
-    }
+    return sum / arr.length;
+  }
 }
-const obj = new Person("Prathamesh","Mahadik",24);
-const obj1 = new Person("Tuhin","Paul",22);
-const obj2 = new Person("Ayush","Soni",23);
-const obj3 = new Person("Ashish","Sangar",25);
-const arr = [obj,obj1,obj2,obj3];
+const obj = new Person("Prathamesh", "Mahadik", 24);
+const obj1 = new Person("Tuhin", "Paul", 22);
+const obj2 = new Person("Ayush", "Soni", 23);
+const obj3 = new Person("Ashish", "Sangar", 25);
+const arr = [obj, obj1, obj2, obj3];
 arr.forEach((e) => {
-    e.fullName()
-    // console.log(`${e.firstName} ${e.lastName} ${e.age}`);
-  });
+  e.fullName();
+});
 const avg = obj.averageAge(arr);
-console.log("Average age:",avg);
+console.log("Average age:", avg);
